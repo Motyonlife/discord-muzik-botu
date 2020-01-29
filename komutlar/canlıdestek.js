6bb66ad1b0a8c6ba68e85cff32a2f39a4835444c
Glitch (tekne) <none>
2020-01-29 10:52:18 +0000

    🌈👱 Checkpoint ./events/ready.js:10757741/83 ./komutlar/canlıdestek.js:10757741/36 ./komutlar/alıntıla.js:10757741/1

--- komutlar/canlıdestek.js
+++ komutlar/canlıdestek.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async (client, message, params) => {
     if(message.channel.type == "dm")  return;
   if(message.channel.type !== "text") return;
-  var channel = client.channels.find('id', '672020749518635022')
+  var channel = client.channels.find('id', '672027091298156544')
     const asdf = await client.channels.get(message.channel.id).createInvite()
   message.delete();
   const embed = new Discord.RichEmbed()
