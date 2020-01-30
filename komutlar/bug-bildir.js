e8ae2c0462b5361fa46b4ee368b96b34082b8d8c
Glitch (tekne) <none>
2020-01-30 11:33:16 +0000

    🚇🛥 Checkpoint ./komutlar/bug-bildir.js:10757741/1393 ./komutlar/mod-log.js:10757741/1711 ./komutlar/çeviri.js:10757741/5607

---
+++ komutlar/bug-bildir.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+let bug = args.join(" ").slice(0);
+let user = message.author.username;
+let guild = message.guild.name;
+let guildid = message.guild.id;
+let kanal = message.channel.name;
+let channel = bot.channels.get("672034510656765962")//bug repot kanal id
+let embed = new Discord.RichEmbed()
+.setTitle("Bug Report")
+.setThumbnail("https://discordapp.com/channels/672020749518635019/672116206677000192/672401811201851392")
+.addField("Bug", bug)
+.addField("Report Eden", user, true)
+.addField("Sunucu", guild, true)
+.addField("Sunucu ID", guildid, true)
+.addField("Kanal", kanal, true)
+.setColor("#f49542")
+
+message.channel.send(":white_check_mark:  **| Bug Report Başarı İle İletildi.**")
+channel.send(embed).then(i => i.react("⏳"))
+
+  
+
+
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0  
+};
+
+exports.help = {
+  name: 'bug-bildir',
+  description: 'Çalışıp para kazanırsınız.',
+  usage: 'bug-bildir'
+}
