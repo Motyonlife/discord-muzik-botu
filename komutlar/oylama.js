a69f42ee406a94531985b9ffa59072690104ee12
Glitch (tekne) <none>
2020-02-07 21:20:58 +0000

    💁💵 Checkpoint ./komutlar/oylama.js:10757741/3

--- komutlar/oylama.js
+++ komutlar/oylama.js
@@ -25,7 +25,7 @@ const Discord = require('discord.js');
        .setColor("RANDOM")
        .setThumbnail(client.user.avatarURL)
        .setTimestamp()
-       .setFooter('Bot', client.user.avatarURL)
+       .setFooter('XQ Bot', client.user.avatarURL)
 
        .addField(`**Oylama**`, `**${question}**`)).then(function(message) {
 
