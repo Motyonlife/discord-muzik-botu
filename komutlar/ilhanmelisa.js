2513eba3df44dff1ed4f8591208fe6d3f7be128a
Glitch (tekne) <none>
2020-02-09 07:18:09 +0000

    🙎🍘 Checkpoint ./package.json:10757741/14314 ./komutlar/ilhanmelisa.js:10757741/556

--- komutlar/ilhanmelisa.js
+++ komutlar/ilhanmelisa.js
@@ -1,18 +1,18 @@
 exports.run = (client,message) => {
 var xir = client.emojis.get("675372467350667294")
-message.channel.send('✨∞ ilhan♥Melisa ∞✨')
+message.channel.send('✨ Botu davet etmek için:⚡️ https://discordapp.com/oauth2/authorize?client_id=675072108232966154&scope=bot&permissions=805314622')
   
 }
   
   module.exports.conf = {
   enabled: true, 
   guildOnly: false, 
-  aliases: ['im', 'ilhanmelisa'],
+  aliases: ['im', 'ilhanmelisa','mi',],
   permLevel: 0 
 };
 
 module.exports.help = {
   name: 'im', 
-  description: '∞ ilhan♥Melisa ∞.',
+  description: 'ilhan & Melisa.',
   usage: 'im'
 };
