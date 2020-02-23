6310f6b4030834621928f86e06060491135032fa
Glitch (tekne) <none>
2020-02-23 19:38:03 +0000

    😋🌆 Checkpoint ./ayarlar.json:10757741/534 ./komutlar/yardım.js:10757741/9

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
   
-  let p = '!'
+  let p = 'x!'
   let arg = args.slice(0).join(' ');
   
   if (!arg[0]) {
