cc4abd9ddc28de0247c680bf1fe9be4718c790f3
Glitch (tekne) <none>
2020-02-28 17:45:59 +0000

    🍄🚒 Checkpoint ./komutlar/müzik.js:10757741/1863 ./komutlar/müzikdetay.js:10757741/993

--- komutlar/müzik.js
+++ komutlar/müzik.js
@@ -1,34 +1,29 @@
 const Discord = require('discord.js');
-
-
-exports.run = function(client, message) {
-const embed = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('Müzik Komutları')
-.setTimestamp()
-.addField('» -Çal- Müzik Açar', '!çal ')
-.addField('» -Sıra- Şarkı Kuyruğunu Gösterir', '!sıra')
-.addField('» -Devam- Şarkıya Devam Eder', '!devam')
-.addField('» -Duraklat- Şarkıyı Durdurur', '!duraklat')
-.addField('» -Kapat- Şarkıyı Kapatır', '!kapat')
-.addField('» -Ses- Müziğe Ses Ve Bass Eklersiniz', '!ses 1/100')
-.addField('» -Çalan- Çalan Şarkının Bilgisini Verir', '!çalan')
-.addField('» -Geç- Şarkıyı Geçersiniz', '!geç')
-.setFooter('Duu6 Müzik Menü', client.user.avatarURL)
-.setTimestamp()
+const ayarlar = require('../ayarlar.json');
+var prefix = ayarlar.prefix;
+exports.run = (client, message, args) => {
+  
+    const juke = new Discord.RichEmbed()
+    .setColor('GOLD')
+    .setAuthor(`Duu6 | Müzik Komutları`, client.user.avatarURL) 
+      .setDescription('**[]()**')
 .setThumbnail(client.user.avatarURL)
-message.channel.send(embed)
+      .addField('**Komutlar:**', '`play`, `stop`, `pause`, `resume`, `volume`, `queue`, `nowplaying`, `skip`,            Detaylı müzik yardım için `!müzikdetay` yazınız.')
+    .setFooter(``, client.user.avatarURL)
+    .setTimestamp()
+    message.channel.send(juke).catch()
+
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false, 
-  aliases: ['müzik','muzik', 'music'], 
-  permLevel: 0 
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
 };
 
 exports.help = {
-  name: 'müzik',
-  description: 'Tüm komutları gösterir.',
-  usage: 'müzik'
+    name: 'müzik',
+      category: 'Yardım',
+      description: 'Yardım kategorilerini gösteir.',
 };
---
+++ komutlar/müzikdetay.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('Müzik Komutları')
+.setTimestamp()
+.addField('» -Çal- Müzik Açar', '!çal ')
+.addField('» -Sıra- Şarkı Kuyruğunu Gösterir', '!sıra')
+.addField('» -Devam- Şarkıya Devam Eder', '!devam')
+.addField('» -Duraklat- Şarkıyı Durdurur', '!duraklat')
+.addField('» -Kapat- Şarkıyı Kapatır', '!kapat')
+.addField('» -Ses- Müziğe Ses Ve Bass Eklersiniz', '!ses 1/100')
+.addField('» -Çalan- Çalan Şarkının Bilgisini Verir', '!çalan')
+.addField('» -Geç- Şarkıyı Geçersiniz', '!geç')
+.setFooter('Duu6 Müzik Menü', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'müzikdetay',
+  description: 'Tüm komutları gösterir.',
+  usage: 'müzikdetau'
+};
