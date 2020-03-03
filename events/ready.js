64e680659e9b1b3beee98bdad57636ae2346147f
Glitch (tekne) <none>
2020-03-03 20:43:17 +0000

    ⛄️📡 Checkpoint ./events/ready.js:11195635/105

--- events/ready.js
+++ events/ready.js
@@ -7,19 +7,20 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "DUU6 Müzik | !komutlar",
+        "Mechanic Müzik",
+        "p!komutlar"
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "DUU6 | !komutlar" );
+        client.user.setActivity(oyun[random], "Mechanic Müzik | p!komutlar" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setActivity(`DUU6 | !komutlar`);
+  client.user.setActivity(`Mechanic Müzik | p!komutlar`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
 };
